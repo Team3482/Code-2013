69f065f4a970a0a7dfb0396d4dbf609f38b4949e
Rodrigo Valle <rdan.valle@gmail.com>
2014-03-14 01:18:50 -0700

    Improved camera subsystem documentation

--- src/robot/core/subsystems/Camera.java
+++ src/robot/core/subsystems/Camera.java
@@ -10,23 +10,27 @@ import edu.wpi.first.wpilibj.camera.AxisCameraException;
 import edu.wpi.first.wpilibj.image.*;
 import edu.wpi.first.wpilibj.Timer;
 
-/**
- *
- * @author
- * Westmont
- * Robotics
- * 
- * Comments were recently updated for our friends at Homestead, good luck!
- */
 public class Camera extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
+	/* The camera subsytem allows for processing of an image to detect both the vertical
+	 * and horizontal vision targets (when present). Image processing here is not real time,
+	 * instead a single image is captured and analyzed, and we assume that only one pair of
+	 * vision targets is visible. The image is captured at the start of autonomous and if both
+	 * targets are visible (the goal is "hot"), the robot scores the ball. Otherwise, if only
+	 * the vertical target is detected, the robot waits five seconds for the hot targets to 
+	 * change and then scores the ball. If neither target is visible, then the robot defaults
+	 * to scoring the ball.
+	 * */
 
+	// Initialize variables used in this subsystem
 	AxisCamera cam;
 	ColorImage colorImg;
 	BinaryImage filtered;
 	BinaryImage sizeFiltered;
 	BinaryImage convexHull;
+	boolean foundHorizontalTarget = false;
+	boolean foundVerticalTarget = false;
+
+	// These are constants for our image filters #vintage #hipster #instagram #yolo
 	final int HUE_LOW = 93;
 	final int HUE_HIGH = 169;
 	final int SATURATION_LOW = 195;
@@ -37,21 +41,26 @@ public class Camera extends Subsystem {
 	final int BRIGHTNESS = 40;
 	final int COLOR_LEVEL = 70;
 	final int COMPRESSION = 20;
-	final double MIN_RECTANGULARITY = 80.0; 
-	final double ASPECT_RATIO_BOUND = 30.0;
+
+	// These constants are used for target detection and thresholding
+	final double VERTICAL_ASPECT_RATIO = 0.125;  // aspect ratio of the vertical target
+	final double HORIZONTAL_ASPECT_RATIO = 5.875;  // aspect ratio fo the horizontal target
+	final double MIN_RECTANGULARITY = 80.0;  // this is a percentage, spectifies a minimum cutoff
+	final double ASPECT_RATIO_BOUND = 20.0;  /* the aspect ratio is a percent error, this variable
+	                                          * bounds it at +/- its value */
+
+	/* These are the settings that we use with the camera, to ensure that camera
+	 * settings do not change and interfere with image processing */
 	final AxisCamera.ExposureT EXPOSURE = AxisCamera.ExposureT.flickerfree60;
 	final AxisCamera.ExposurePriorityT EXPOSURE_PRIORITY = AxisCamera.ExposurePriorityT.imageQuality;
 	final int MAX_FPS = 20;
 	final AxisCamera.ResolutionT RESOLUTION = AxisCamera.ResolutionT.k640x480;
 	final AxisCamera.RotationT ROTATION = AxisCamera.RotationT.k0;
 	final AxisCamera.WhiteBalanceT WHITE_BALANCE = AxisCamera.WhiteBalanceT.fixedIndoor;
-	boolean foundHorizontalTarget = false;
-	boolean foundVerticalTarget = false;
 
-	/* Call initCamera() here so that (ideally) the robot is ready to go before autonomous
-	 * mode is instantiated
-	 */
 	public Camera() {
+		/* Call initCamera() here so that (ideally) the robot is ready to go before autonomous
+		 * mode is instantiated */
 		try {
 			initCamera();
 		} catch (Exception e) {
@@ -64,17 +73,19 @@ public class Camera extends Subsystem {
 		//setDefaultCommand(new MySpecialCommand());
 	}
 	
-	/* Call this function before trying to process images, this only
-	 * has to be called once, and only once. It takes too much time
-	 * to run, so consider running this before the match begins */
 	public void initCamera() throws AxisCameraException {
+		/* Call this function before trying to process images, this only
+		 * has to be called once, and only once. It takes too much time
+		 * to run, so consider running this before the match begins */
+
 		long startTime = System.currentTimeMillis();  // this is for timing purposes
 		cam = AxisCamera.getInstance(IP_ADDRESS);  // grab the camera at the default IP
 		while (!cam.freshImage()) {  // wait for the camera to finish starting up
 			Timer.delay(0.1);
 		}
-		// Set up the camera settings to make sure they're consistent
-		// This takes a *lot* of time comparatively
+
+		/* Set up the camera settings to make sure they're consistent.
+		 * This takes a *lot* of time (about six to ten seconds) */
 		cam.writeBrightness(BRIGHTNESS);
 		cam.writeColorLevel(COLOR_LEVEL);
 		cam.writeCompression(COMPRESSION);
@@ -90,8 +101,9 @@ public class Camera extends Subsystem {
 		System.out.println("Initialization Time: " + (endTime - startTime));
 	}
 
-	/* This grabs a single image, processes it, and stores the particle scores in a local array */
 	public void processImage() throws AxisCameraException, NIVisionException {
+		/* This grabs a single image, processes it, and stores the particle scores in a local array */
+
 		// For timing function execution
 		long startTime = System.currentTimeMillis();
 		
@@ -104,11 +116,11 @@ public class Camera extends Subsystem {
 		filtered.write("/tmp/filtered_image.png");
 		
 		/* Filter blobs by size, save it to a different image variable otherwise it will cause a memory leak.
-		   For example, the line
-			   filtered = filtered.removeSmallObjects(...) 
-		   would cause a leak because the image data originally stored in filtered is not freed.
-		   Do not do this, save your image data in a separate variable and make sure to free all image
-		   variables later. */     
+		 * For example, the line
+		 *	   filtered = filtered.removeSmallObjects(...) 
+		 *   would cause a leak because the image data originally stored in filtered is not freed.
+		 *   Do not do this, save your image data in a separate variable and make sure to free all image
+		 * variables later. */     
 		sizeFiltered = filtered.removeSmallObjects(true, 3);
 		sizeFiltered.write("/tmp/size_filtered.png");
 		
@@ -126,7 +138,7 @@ public class Camera extends Subsystem {
 		endTime = System.currentTimeMillis();
 		System.out.println("Score Processing Time: " + (endTime - startTime));
 
-		// free image variables
+		// Free image variables
 		colorImg.free();
 		filtered.free();
 		sizeFiltered.free();
@@ -134,6 +146,8 @@ public class Camera extends Subsystem {
 	}
 
 	public boolean foundVertical() {
+		/* Returns the corresponding boolean variable set in calculateScores() */
+
 		return foundVerticalTarget;
 	}
 
@@ -146,8 +160,7 @@ public class Camera extends Subsystem {
 	 **************************/
 	private void calculateScores(BinaryImage img) throws AxisCameraException, NIVisionException {
 		/* Calculate the rectangularity score as a percentage, and the vertical/horizontal
-		 * aspect ratios
-		 */
+		 * aspect ratios */
 
 		// getOrdered...() returns particles ordered from greatest area to least
 		ParticleAnalysisReport[] reports = img.getOrderedParticleAnalysisReports();
@@ -161,33 +174,35 @@ public class Camera extends Subsystem {
 			scoredBlobs[i][2] = scoreAspectRatioHorizontal(reports[i]);
 
 			/* If any scored blobs are detected as horizontal or vertical at any point, 
-			 * the corresponding variables will stay true */
+			 * the corresponding variables will stay true (they're initialized false) */
 			foundHorizontalTarget = foundHorizontalTarget || isHorizontal(scoredBlobs[i]);
 			foundVerticalTarget = foundVerticalTarget || isVertical(scoredBlobs[i]);
 		}
 	}
 
 	private double scoreRectangularity(ParticleAnalysisReport blob) {
-		// returns rectangularity score as a value from 0 to 100, with 100 being the best
-		return blob.particleArea / (blob.boundingRectWidth * blob.boundingRectHeight) * 100;
+		/* Returns rectangularity score as a value from 0 to 100, with 100 being the best */
 
+		return blob.particleArea / (blob.boundingRectWidth * blob.boundingRectHeight) * 100;
 	}
 
 	private double scoreAspectRatioVertical(ParticleAnalysisReport blob) {
-		// returns vertical aspect ratio score as a percent error, the closer to zero the better
-		final double VERTICAL_ASPECT_RATIO = 0.125;
+		/* Returns vertical aspect ratio score as a percent error, the closer to zero the better */
+
 		double blobAspectRatio = (double) blob.boundingRectWidth / blob.boundingRectHeight;
 		return Math.abs(VERTICAL_ASPECT_RATIO - blobAspectRatio) / VERTICAL_ASPECT_RATIO * 100;
 	}
 	
 	private double scoreAspectRatioHorizontal(ParticleAnalysisReport blob) {
-		// returns horizontal aspect ratio score as a percent error, the closer to zero the better
-		final double HORIZONTAL_ASPECT_RATIO = 5.875;
+		/* Returns horizontal aspect ratio score as a percent error, the closer to zero the better */
+
 		double blobAspectRatio = (double) blob.boundingRectWidth / blob.boundingRectHeight;
 		return Math.abs(HORIZONTAL_ASPECT_RATIO - blobAspectRatio) / HORIZONTAL_ASPECT_RATIO * 100;
 	}
 	
 	private boolean isVertical(double[] scoredBlob) {
+		/* Determines whether a scored particle falls within the threshold, and prints debugging information */
+
 		if (scoredBlob[0] > MIN_RECTANGULARITY && scoredBlob[1] < ASPECT_RATIO_BOUND) {
 			System.out.println("Vertical Target Matched");
 			System.out.println("Rectangularity, Aspect Ratio");
@@ -202,7 +217,7 @@ public class Camera extends Subsystem {
 	
 	private boolean isHorizontal(double[] scoredBlob) {
 		if (scoredBlob[0] > MIN_RECTANGULARITY && scoredBlob[2] < ASPECT_RATIO_BOUND) {
-			System.out.println("Vertical Target Matched");
+			System.out.println("Horizontal Target Matched");
 			System.out.println("Rectangularity, Aspect Ratio");
 			System.out.println(scoredBlob[0] + ", " + scoredBlob[2]);
 			return true;
